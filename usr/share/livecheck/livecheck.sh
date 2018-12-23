17e98ac1943d7d8256ab24e65f6abe50629d0ff3
Algernon-01 <33966997+Algernon-01@users.noreply.github.com>
2018-12-23 19:33:15 +0000

    Open browser on click.

--- usr/share/livecheck/livecheck.sh
+++ usr/share/livecheck/livecheck.sh
@@ -12,15 +12,20 @@ if grep -qs "boot=live" /proc/cmdline; then
       # disk is set to RO, too
       echo "<img>/usr/share/icons/gnome-colors-common/16x16/actions/dialog-apply.png</img>"
       echo "<txt>Live</txt>"
-      echo "<tool>Live mode is enabled. All changes to the disk will be gone after a reboot. See: https://whonix.org/wiki/Whonix_Live</tool>"
+      echo "<tool>Live mode is enabled. All changes to the disk will be gone after a reboot. See: https://whonix.org/wiki/Whonix_Live or click on the icon for more information.</tool>"
+      echo "<click>bash /home/user/.tb/tor-browser/Browser/start-tor-browser --allow-remote https://whonix.org/wiki/Whonix_Live</click>"
+      echo "<txtclick>bash /home/user/.tb/tor-browser/Browser/start-tor-browser --allow-remote https://whonix.org/wiki/Whonix_Live</txtclick>" 
    else
       # disk is not set to RO
       echo "<img>/usr/share/icons/Adwaita/16x16/status/dialog-warning.png</img>"
       echo "<txt>Live</txt>"
-      echo "<tool>Live mode is enabled but it is still possible to write to the disk. Please power off the machine and set the disk to read-only. See: https://whonix.org/wiki/Whonix_Live</tool>"
+      echo "<tool>Live mode is enabled but it is still possible to write to the disk. Please power off the machine and set the disk to read-only. See: https://whonix.org/wiki/Whonix_Live or click on the icon for more information.</tool>"
+      echo "<click>bash /home/user/.tb/tor-browser/Browser/start-tor-browser --allow-remote https://whonix.org/wiki/Whonix_Live</click>" 
+      echo "<txtclick>bash /home/user/.tb/tor-browser/Browser/start-tor-browser --allow-remote https://whonix.org/wiki/Whonix_Live</txtclick>"
    fi
 else
    # live mode is disabled
    echo "<img>/usr/share/icons/gnome-colors-common/22x22/status/gtk-info.png</img>"
-   echo "<tool>You are using persistent mode. All changes to the disk will be preserved after a reboot. For using live mode, see: https://whonix.org/wiki/Whonix_Live</tool>"
+   echo "<tool>You are using persistent mode. All changes to the disk will be preserved after a reboot. For using live mode, see: https://whonix.org/wiki/Whonix_Live or click on the icon for more information.</tool>"
+   echo "<click>bash /home/user/.tb/tor-browser/Browser/start-tor-browser --allow-remote https://whonix.org/wiki/Whonix_Live</click>"
 fi
